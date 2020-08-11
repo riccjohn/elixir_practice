0f5ca2e5648de7cf4ed0deb8671f8d3beec12ee1
riccjohn <riccjohn@gmail.com>
2020-08-11 15:47:40 -0500

    Remove _build, add guess function

--- Guessing_Game/lib/guessing_game.ex
+++ Guessing_Game/lib/guessing_game.ex
@@ -1,12 +1,26 @@
 defmodule GuessingGame do
-  # guess between low and high number -> guess middle
-  # tell user our guess
-  # "yes" -> game over
-  # "bigger" -> bigger(low, high)
-  # "smaller" -> smaller(low, high)
-  # anything else -> tell user to enter a valid response
-
+  @spec mid(integer, integer) :: integer
   def mid(low, high) do
     div(low + high, 2)
+  end
+
+  def guess(low, high) do
+    middle_guess = mid(low, high)
+    answer = IO.gets("Hmm ... maybe you're thinking of #{middle_guess}?\n")
+
+    case String.trim(answer) do
+      "bigger" ->
+        guess(middle_guess, high)
+
+      "smaller" ->
+        guess(low, middle_guess)
+
+      "yes" ->
+        "I knew I'd get it!"
+
+      _ ->
+        IO.puts(~s{Type "bigger", "smaller", or "yes"})
+        guess(low, high)
+    end
   end
 end

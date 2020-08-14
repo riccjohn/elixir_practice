d199dd66ebad5d45a507ad34aadcdf8cae3c9392
John Riccardi <riccjohn@gmail.com>
2020-08-14 15:12:20 -0500

    Add func to read from file

--- MinimalToDo/minimal_todo.ex
+++ MinimalToDo/minimal_todo.ex
@@ -1,3 +1,20 @@
 defmodule MinimalTodo do
+  def start do
+    filename = IO.gets("Name of .csv to load: ") |> String.trim
+    read(filename)
+    # ask user for filename
+    # open file and read
+    # parse the data
+    # ask user for command
+    # (read todos, add todos, delete todos, load file, save files)
 
+  end
+
+  def read(filename) do
+     case File.read(filename) do
+       {:ok, body} -> body
+       {:error, reason} ->  IO.puts ~s(Could not open file "#{filename}"\n)
+                            IO.inspect reason
+     end
+  end
 end
